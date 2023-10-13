d269ffec4381bd87ffb54ce77a91c7d159607e58
alexseik <alex.garrido.lopez@gmail.com>
2023-10-13 18:13:37 +0200

    feat: finish day 1

--- src/app/components/candidate/candidate.component.ts
+++ src/app/components/candidate/candidate.component.ts
@@ -1,16 +1,64 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Candidate } from '../../models/candidate.model';
 
 @Component({
   selector: 'app-candidate',
   templateUrl: './candidate.component.html',
   styleUrls: ['./candidate.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class CandidateComponent {
+export class CandidateComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input() candidate!: Candidate;
 
+  @Output() select = new EventEmitter<Candidate>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes', changes);
+  }
+  ngOnInit(): void {
+    console.log('OnInit', { candidate: this.candidate });
+  }
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+
   doEdit = () => {
-    alert(`You want to edit this candidate: ${this.candidate.name}`);
+    this.select.emit(this.candidate);
   };
 
   candidateClasses() {
